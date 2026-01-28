6163379691b618cdcaf670b7dde64acc5f11118c
David Nefdt <david.nefdt@gmail.com>
2026-01-28 14:47:50 +0200

    Tweaks

--- Agent/obj/Debug/net8.0-windows/Agent.AssemblyInfo.cs
+++ Agent/obj/Debug/net8.0-windows/Agent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed5a95f33fa84a60bd204a2f9e7d6cfe0a245961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776fe7b603c39e520815796884c71345689c61d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
